fe22a7de00b7d73e5eebd61195a4d781cfb0014b
Dimitar Rupov <dimitar@precessionmedia.com>
2019-01-21 10:12:00 +0100

    Add a node script

---
+++ scripts/node.sh
@@ -0,0 +1,27 @@
+# The site name should be passed in as a first parameter to the shell script.
+appName=$1
+
+mkdir $appName
+cd $appName
+
+echo "name: $appName" >> .lando.yml
+echo "services:" >> .lando.yml
+echo "  appserver:" >> .lando.yml
+echo "    type: node:8.9" >> .lando.yml
+echo "    overrides:" >> .lando.yml
+echo "      services:" >> .lando.yml
+echo "        ports:" >> .lando.yml
+echo "          - 3000:3000" >> .lando.yml
+echo "    globals:" >> .lando.yml
+echo '      gulp-cli: "latest"' >> .lando.yml
+echo "tooling:" >> .lando.yml
+echo "  npm:" >> .lando.yml
+echo "    service: appserver" >> .lando.yml
+echo "  node:" >> .lando.yml
+echo "    service: appserver" >> .lando.yml
+echo "  yarn:" >> .lando.yml
+echo "    service: appserver" >> .lando.yml
+echo "  gulp:" >> .lando.yml
+echo "    service: appserver" >> .lando.yml
+
+lando start
