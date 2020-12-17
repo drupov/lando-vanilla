664cfb11c3423a907cc3ca2c7501a65927ce0a5a
Dimitar Rupov <dimitar.rupov@gmail.com>
2020-12-17 18:12:09 +0100

    Do not install webprofiler in D9 for now

--- scripts/drupal9.sh
+++ scripts/drupal9.sh
@@ -64,7 +64,7 @@ chmod 755 web/sites/default
 
 lando composer require drupal/coffee drupal/admin_toolbar
 lando composer require drupal/devel drupal/module_filter --dev
-lando drush pm-enable coffee admin_toolbar_tools devel devel_generate webprofiler module_filter --yes
+lando drush pm-enable coffee admin_toolbar_tools devel devel_generate module_filter --yes
 
 echo "Browse your site by visiting:"
 lando info | grep lndo.site
