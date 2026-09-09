c98c0c2801f42e63b7dc516bb6571cecb6d0905d
Dimitar Rupov <dimitar.rupov@gmail.com>
2026-09-09 12:57:36 +0200

    Restore php

--- scripts/php.sh
+++ scripts/php.sh
@@ -5,28 +5,25 @@ source "$scriptDir/_bootstrap.sh"
 
 prepare_app_directory "n:s" "$@"
 
-# echo "name: $appName" >> .lando.yml
-# echo "recipe: lamp" >> .lando.yml
-# echo "config:" >> .lando.yml
-# echo "  webroot: ." >> .lando.yml
-# echo "  xdebug: true" >> .lando.yml
-# echo "  php: 8.1" >> .lando.yml
-# echo "services:" >> .lando.yml
-# echo "  appserver:" >> .lando.yml
-# echo "    overrides:" >> .lando.yml
-# echo "      environment:" >> .lando.yml
-# echo "        PHP_IDE_CONFIG: 'serverName=appserver'" >> .lando.yml
-# echo "    config:" >> .lando.yml
-# echo "      php: php.ini" >> .lando.yml
+echo "name: $appName" >> .lando.yml
+echo "services:" >> .lando.yml
+echo "  appserver:" >> .lando.yml
+echo "    type: php:8.5" >> .lando.yml
+echo "    via: cli" >> .lando.yml
+echo "    xdebug: true" >> .lando.yml
+echo "    config:" >> .lando.yml
+echo "      php: php.ini" >> .lando.yml
+echo "tooling:" >> .lando.yml
+echo "  php:" >> .lando.yml
+echo "    service: appserver" >> .lando.yml
+echo "  composer:" >> .lando.yml
+echo "    service: appserver" >> .lando.yml
 
 echo "memory_limit = 128M" >> php.ini
 echo "xdebug.start_with_request = 1" >> php.ini
 echo "xdebug.log_level = 0" >> php.ini
 
-echo "<?php" >> index.php
-echo "" >> index.php
-echo "print 'Hello world';" >> index.php
-
 lando start
 
-echo "PHP playground is installed and available at: https://$appName.lndo.site"
+echo "PHP is available in $appName with: lando php"
+echo "Composer is available in $appName with: lando composer"
